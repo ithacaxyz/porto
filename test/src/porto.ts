f303d6ed9ee83c54423b19a43a33b9e39684bb70
jxom <7336481+jxom@users.noreply.github.com>
2025-01-09 08:29:39 +1100

    up

--- test/src/porto.ts
+++ test/src/porto.ts
@@ -28,7 +28,7 @@ export const createPorto = () =>
   })
 
 export const porto = createPorto()
-export const client = Porto.getClient(porto).extend(() => ({
+export const client = Porto.getClients(porto).default.extend(() => ({
   mode: 'anvil',
 }))
 export const delegation = client.chain.contracts.delegation.address
