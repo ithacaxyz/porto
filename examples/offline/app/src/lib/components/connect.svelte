ff53f48c3ba041a5f6d30cf272e8528b87034985
omar <omaraziz.dev@proton.me>
2025-01-18 22:16:42 -0800

    chore: checkpoint

--- examples/offline/app/src/lib/components/connect.svelte
+++ examples/offline/app/src/lib/components/connect.svelte
@@ -1,34 +1,41 @@
 <script lang="ts">
-  import { porto, wagmiConfig } from '$/lib/config.ts'
-  import { createQuery, useQueryClient } from '@tanstack/svelte-query'
-  import { disconnect, getAccount, getConnectors, type GetAccountReturnType } from '@wagmi/core'
+import { porto, wagmiConfig } from '$/lib/config.ts'
+import { createQuery, useQueryClient } from '@tanstack/svelte-query'
+import {
+  type GetAccountReturnType,
+  disconnect,
+  getAccount,
+  getConnectors,
+} from '@wagmi/core'
 
-  const queryClient = useQueryClient()
+const queryClient = useQueryClient()
 
-  const account = createQuery<GetAccountReturnType>({
-    queryKey: ['account'],
-    queryFn: () => getAccount(wagmiConfig),
-  })
-  let connected = $derived($account?.data?.isConnected)
+const account = createQuery<GetAccountReturnType>({
+  queryKey: ['account'],
+  queryFn: () => getAccount(wagmiConfig),
+})
+let connected = $derived($account?.data?.isConnected)
 
-  async function connectWallet() {
-    if (connected) await disconnect(wagmiConfig)
-    else {
-      const connectors = getConnectors(wagmiConfig)
-      const portoConnector = connectors.filter(connector => connector.id === 'xyz.ithaca.porto')
-      const connectResult = await porto.provider.request({
-        method: 'wallet_connect',
-        params: [
-          {
-            capabilities: {
-              createAccount: true,
-            },
+async function connectWallet() {
+  if (connected) await disconnect(wagmiConfig)
+  else {
+    const connectors = getConnectors(wagmiConfig)
+    const portoConnector = connectors.filter(
+      (connector) => connector.id === 'xyz.ithaca.porto',
+    )
+    const connectResult = await porto.provider.request({
+      method: 'wallet_connect',
+      params: [
+        {
+          capabilities: {
+            createAccount: true,
           },
-        ],
-      })
-      console.info(`${connectResult.accounts.at(0)} connected`)
-    }
+        },
+      ],
+    })
+    console.info(`${connectResult.accounts.at(0)} connected`)
   }
+}
 </script>
 
 <button type="button" onclick={connectWallet} class="hover:cursor-pointer mr-auto">
