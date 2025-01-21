c1fd29765df5504fb3e1118f9e77b1274d61e183
omar <omaraziz.dev@proton.me>
2025-01-20 18:08:45 -0800

    chore: cleanup

--- examples/offline/server/src/main.ts
+++ examples/offline/server/src/main.ts
@@ -6,7 +6,6 @@ import { env, getRuntimeKey } from 'hono/adapter'
 import { contextStorage, getContext } from 'hono/context-storage'
 import { cors } from 'hono/cors'
 import { showRoutes } from 'hono/dev'
-import { logger } from 'hono/logger'
 import { Address, Json, type RpcSchema, WebCryptoP256 } from 'ox'
 import type { Wallet } from 'ox/RpcSchema'
 
@@ -15,23 +14,15 @@ type WalletSendCallsParams = RpcSchema.ExtractRequest<
   'wallet_sendCalls'
 >['params'][number]
 
-type Calls = Omit<WalletSendCallsParams['calls'][number], 'value'> & {
-  value?: bigint | undefined
-}
-
 type SendCallsContext = WalletSendCallsParams
-//  Omit<WalletSendCallsParams, 'calls'> & {
-//   calls: Calls[]
-// }
 
 const app = new Hono<Env>()
 
-app.use('*', logger())
 app.use(contextStorage())
 app.use('*', cors({ origin: '*', allowMethods: ['GET', 'HEAD', 'OPTIONS'] }))
 
 app.use(async (context, next) => {
-  context.set('KEY_PAIR', 'test')
+  context.set('KEY_PAIR', context.get('KEY_PAIR'))
   await next()
 })
 
@@ -47,10 +38,6 @@ app.get('/routes', async (context) => {
   }
   return new Response(null, { status: 418 })
 })
-
-app.get('/v1', (context) =>
-  context.json({ version: '1', name: 'Porto Offline Tx Server' }),
-)
 
 /**
  * Returns existing keys
@@ -75,31 +62,18 @@ app.post('/keys', async (context) => {
   })
 
   context.set('KEY_PAIR', Json.stringify(keyPair))
-
-  // getContext<{
-  //   Bindings: { KV: KVNamespace }
-  // }>().env.KV.put('keyPair', JSON.stringify(keyPair))
-
   return context.json(Key.toRpc(key))
 })
 
 /**
  * once the app client authorizes the key,
- * it will call this endpoint to notify the server
+ * it will call this endpoint to notify the server (WIP)
  */
 app.post('/authorize-status', async (context) => {
   const payload = await context.req.json<{}>()
-  console.info(JSON.stringify(payload, undefined, 2))
-  // const keyPairString = await getContext<{
-  //   Bindings: { KV: KVNamespace }
-  // }>().env.KV.get('keyPair')
-  // const keyPair = Json.parse(keyPairString ?? '{}') as Key.Key
-  // const keyPairString = context.get('KEY_PAIR') as Key.Key
-  // console.info()
-  console.info('key pairs')
+  console.info(context.get('KEY_PAIR'), getContext<Env>().get('KEY_PAIR'))
   const keyPair = context.get('KEY_PAIR')
   console.info(JSON.stringify({ keyPair }, undefined, 2))
-  const keyPairString = {}
 
   const porto = Porto.create({
     chains: [Chains.odysseyTestnet],
@@ -144,6 +118,8 @@ app.post('/authorize-status', async (context) => {
       },
     ],
   })
+
+  // TODO: `Delegation.execute(calls, signature)`
 
   return context.json({ error: 'Not implemented' })
 })
