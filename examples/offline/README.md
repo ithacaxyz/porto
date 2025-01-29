1c25930a244bcc07e2f04b7ff88cd87d6ee44fd4
omar <omaraziz.dev@proton.me>
2025-01-29 00:12:32 -0800

    feat: live instance

---
+++ examples/offline/README.md
@@ -0,0 +1,14 @@
+# Demo: Delegate Porto transactions to a server
+
+
+### Run locally
+
+run server
+```bash
+pnpm --filter offline-server-example dev
+```
+
+run app
+```bash
+pnpm --filter offline-app-example dev
+```
--- examples/offline/app/vite.config.ts
+++ examples/offline/app/vite.config.ts
@@ -1,31 +1,6 @@
-import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
-import { createServer } from 'prool'
-import { anvil } from 'prool/instances'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'anvil',
-      async configureServer() {
-        await createServer({
-          instance(key) {
-            return anvil({
-              chainId: key,
-              forkUrl: 'https://eth.merkle.io',
-              hardfork: 'Prague',
-              // @ts-ignore
-              odyssey: true,
-              loadState: resolve(import.meta.dirname, './anvil.json'),
-              dumpState: resolve(import.meta.dirname, './anvil.dump.json'),
-            })
-          },
-          port: 8545,
-        }).start()
-      },
-    },
-  ],
+  plugins: [react()],
 })
