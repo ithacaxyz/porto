8853e20e40baafbc86476bdb3a4d2d074619301d
jxom <7336481+jxom@users.noreply.github.com>
2025-02-13 17:05:58 +1100

    chore: up

--- src/core/internal/typebox/schema.ts
+++ src/core/internal/typebox/schema.ts
@@ -15,24 +15,28 @@ export { Value, Encode, Decode } from '@sinclair/typebox/value'
 
 import type { DeepReadonly, OneOf as OneOfType } from '../types.js'
 
-type OneOfStatic<T extends TSchema[], P extends unknown[]> = {
-  [K in keyof T]: T[K] extends TSchema ? Static_typebox<T[K], P> : never
-}[number]
-export interface TOneOf<T extends TSchema[] = TSchema[]> extends TSchema {
-  [Kind]: 'Union'
-  // @ts-expect-error
-  static: OneOfType<OneOfStatic<T, this['params']>>
-}
-export type OneOf<T extends TSchema[]> = T extends []
-  ? TNever
-  : T extends [TSchema]
-    ? T[0]
-    : TOneOf<T>
-
 export function OneOf<schemas extends TSchema[]>(
   schemas: [...schemas],
-): TOneOf<schemas> {
+): OneOf.ReturnType<schemas> {
   return Type.Union(schemas) as never
+}
+
+export declare namespace OneOf {
+  type OneOfStatic<T extends TSchema[], P extends unknown[]> = {
+    [K in keyof T]: T[K] extends TSchema ? Static_typebox<T[K], P> : never
+  }[number]
+
+  export interface ReturnType<T extends TSchema[] = TSchema[]> extends TSchema {
+    [Kind]: 'Union'
+    // @ts-expect-error
+    static: OneOfType<OneOfStatic<T, this['params']>>
+  }
+
+  export type OneOf<T extends TSchema[]> = T extends []
+    ? TNever
+    : T extends [TSchema]
+      ? T[0]
+      : ReturnType<T>
 }
 
 export function Optional<schema extends TSchema>(
