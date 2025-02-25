7a1f6dcd657d37bf14364bef70555bdb99a9d274
omar <omaraziz.dev@proton.me>
2025-02-25 09:54:22 -0800

    chore: checkpoint

--- app/src/components/Pill.tsx
+++ app/src/components/Pill.tsx
@@ -1,6 +1,6 @@
 export function Pill({ children }: { children: React.ReactNode }) {
   return (
-    <span className="rounded-sm bg-gray6 px-1.5 py-0.5 font-light text-gray9 text-xs tracking-tighter">
+    <span className="rounded-sm bg-gray6 px-1.5 py-0.5 font-light text-gray9 text-xs tracking-tighter dark:text-gray-50">
       {children}
     </span>
   )
