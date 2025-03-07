afa23e369bd60ae6b3125c90cb6de5959e8a5066
jxom <7336481+jxom@users.noreply.github.com>
2025-03-07 13:29:33 +1100

    up

--- src/core/RpcSchema.ts
+++ src/core/RpcSchema.ts
@@ -1,56 +1,80 @@
 import type * as RpcSchema from 'ox/RpcSchema'
 
+import type { PublicRpcSchema } from 'viem'
 import type * as Rpc from './internal/typebox/rpc.js'
 import type { Static } from './internal/typebox/schema.js'
-import type { DeepReadonly } from './internal/types.js'
+import type { DeepReadonly, UnionToTuple } from './internal/types.js'
 
-export type Schema = RpcSchema.From<
+export type Schema =
+  | RpcSchema.Eth
   | Exclude<
-      DeepReadonly<RpcSchema.Default>,
+      DeepReadonly<RpcSchema.Wallet>,
       { Request: { method: 'wallet_sendCalls' } }
     >
-  | {
-      Request: Static<typeof Rpc.porto_ping.Request>
-      ReturnType: Static<typeof Rpc.porto_ping.Response>
-    }
-  | {
-      Request: Static<typeof Rpc.experimental_createAccount.Request>
-      ReturnType: Static<typeof Rpc.experimental_createAccount.Response>
-    }
-  | {
-      Request: Static<typeof Rpc.experimental_grantPermissions.Request>
-      ReturnType: Static<typeof Rpc.experimental_grantPermissions.Response>
-    }
-  | {
-      Request: Static<typeof Rpc.experimental_prepareCreateAccount.Request>
-      ReturnType: Static<typeof Rpc.experimental_prepareCreateAccount.Response>
-    }
-  | {
-      Request: Static<typeof Rpc.experimental_permissions.Request>
-      ReturnType: Static<typeof Rpc.experimental_permissions.Response>
-    }
-  | {
-      Request: Static<typeof Rpc.experimental_revokePermissions.Request>
-      ReturnType: undefined
-    }
-  | {
-      Request: Static<typeof Rpc.wallet_connect.Request>
-      ReturnType: Static<typeof Rpc.wallet_connect.Response>
-    }
-  | {
-      Request: Static<typeof Rpc.wallet_disconnect.Request>
-      ReturnType: undefined
-    }
-  | {
-      Request: Static<typeof Rpc.wallet_prepareCalls.Request>
-      ReturnType: Static<typeof Rpc.wallet_prepareCalls.Response>
-    }
-  | {
-      Request: Static<typeof Rpc.wallet_sendPreparedCalls.Request>
-      ReturnType: Static<typeof Rpc.wallet_sendPreparedCalls.Response>
-    }
-  | {
-      Request: Static<typeof Rpc.wallet_sendCalls.Request>
-      ReturnType: Static<typeof Rpc.wallet_sendCalls.Response>
-    }
->
+  | RpcSchema.From<
+      | {
+          Request: Static<typeof Rpc.porto_ping.Request>
+          ReturnType: Static<typeof Rpc.porto_ping.Response>
+        }
+      | {
+          Request: Static<typeof Rpc.experimental_createAccount.Request>
+          ReturnType: Static<typeof Rpc.experimental_createAccount.Response>
+        }
+      | {
+          Request: Static<typeof Rpc.experimental_grantPermissions.Request>
+          ReturnType: Static<typeof Rpc.experimental_grantPermissions.Response>
+        }
+      | {
+          Request: Static<typeof Rpc.experimental_prepareCreateAccount.Request>
+          ReturnType: Static<
+            typeof Rpc.experimental_prepareCreateAccount.Response
+          >
+        }
+      | {
+          Request: Static<typeof Rpc.experimental_permissions.Request>
+          ReturnType: Static<typeof Rpc.experimental_permissions.Response>
+        }
+      | {
+          Request: Static<typeof Rpc.experimental_revokePermissions.Request>
+          ReturnType: undefined
+        }
+      | {
+          Request: Static<typeof Rpc.wallet_connect.Request>
+          ReturnType: Static<typeof Rpc.wallet_connect.Response>
+        }
+      | {
+          Request: Static<typeof Rpc.wallet_disconnect.Request>
+          ReturnType: undefined
+        }
+      | {
+          Request: Static<typeof Rpc.wallet_prepareCalls.Request>
+          ReturnType: Static<typeof Rpc.wallet_prepareCalls.Response>
+        }
+      | {
+          Request: Static<typeof Rpc.wallet_sendPreparedCalls.Request>
+          ReturnType: Static<typeof Rpc.wallet_sendPreparedCalls.Response>
+        }
+      | {
+          Request: Static<typeof Rpc.wallet_sendCalls.Request>
+          ReturnType: Static<typeof Rpc.wallet_sendCalls.Response>
+        }
+    >
+
+export type ToViem = [
+  ...PublicRpcSchema,
+  ...MapSchema<UnionToTuple<Exclude<Schema, RpcSchema.Eth>>>,
+]
+
+export type MapSchema<array extends readonly unknown[]> = array extends [
+  infer head extends RpcSchema.Generic,
+  ...infer tail extends RpcSchema.Generic[],
+]
+  ? [
+      {
+        Method: head['Request']['method']
+        Parameters: head['Request']['params']
+        ReturnType: head['ReturnType']
+      },
+      ...MapSchema<tail>,
+    ]
+  : []
