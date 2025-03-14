f4efbe4216ba19f245d52d4595cf340c4c7f2e21
omar <omaraziz.dev@proton.me>
2025-03-14 04:04:33 -0700

    chore: always cors bypass

--- app/src/hooks/use-address-token-balances.ts
+++ app/src/hooks/use-address-token-balances.ts
@@ -1,85 +1,76 @@
 import { useQuery } from '@tanstack/react-query'
 import { Address } from 'ox'
-import { useAccount, useBalance, useChainId } from 'wagmi'
+import { useAccount, useBalance } from 'wagmi'
 
 import { addressApiEndpoint, urlWithLocalCorsBypass } from '~/lib/Constants'
 import { config } from '~/lib/Wagmi'
 
 export function useTokenBalances({
-  chainId: providedChainId,
   address,
 }: {
-  chainId?: number
   address?: Address.Address
 } = {}) {
   const account = useAccount()
-  const connectedChainId = useChainId()
-
-  const chainId = providedChainId ?? connectedChainId
   const userAddress = address ?? account.address
 
-  const { data: balance } = useBalance({
+  const { data: balance, status: gasStatus } = useBalance({
     address: userAddress,
   })
 
-  const { data, status, error, refetch, isError, isSuccess, isPending } =
-    useQuery({
-      queryKey: ['token-balances', userAddress],
-      enabled: !!userAddress && Address.validate(userAddress),
-      queryFn: async () => {
-        try {
-          // if no chainId then fetch from all 3 chains
-          if (!chainId) {
-            const responses = await Promise.all(
-              config.chains.map(async (chain) => {
-                const apiEndpoint = addressApiEndpoint(chain.id)
-                const url = `${apiEndpoint}/addresses/${userAddress}/token-balances`
-                const response = await fetch(url)
-                if (!response.ok) {
-                  throw new Error(
-                    `Failed to fetch token balances: ${response.statusText}`,
-                  )
-                }
-                return { id: chain.id, data: await response.json() }
-              }),
-            )
-            const data = responses.map((response) => response.data)
+  const {
+    data: tokenBalances,
+    status,
+    error,
+    refetch,
+    isError,
+    isSuccess,
+    isPending,
+  } = useQuery({
+    queryKey: ['token-balances', userAddress],
+    enabled:
+      !!userAddress && Address.validate(userAddress) && gasStatus === 'success',
+    queryFn: async () => {
+      try {
+        // return data
+        const responses = await Promise.all(
+          config.chains.map(async (chain) => {
+            const apiEndpoint = addressApiEndpoint(chain.id)
+            const url = `${apiEndpoint}/addresses/${userAddress}/token-balances`
+            const response = await fetch(urlWithLocalCorsBypass(url))
+            if (!response.ok) {
+              throw new Error(
+                `Failed to fetch token balances: ${response.statusText}`,
+              )
+            }
+            return { id: chain.id, data: await response.json() }
+          }),
+        )
+        const data = responses.map((response) => response.data)
 
-            return data as Array<TokenBalance>
-          }
-
-          const apiEndpoint = addressApiEndpoint(chainId)
-          const url = `${apiEndpoint}/addresses/${userAddress}/token-balances`
-          const response = await fetch(urlWithLocalCorsBypass(url))
-          if (!response.ok) {
-            throw new Error(
-              `Failed to fetch token balances: ${response.statusText}`,
-            )
-          }
-          const data = await response.json()
-          return data as Array<TokenBalance>
-        } catch (error) {
-          console.error('Error fetching token balances:', error)
-          throw error
-        }
-      },
-      select: (data) =>
-        [
-          ...(data ?? []),
-          {
-            value: balance?.value,
-            token: {
-              decimals: balance?.decimals,
-              name: balance?.symbol,
-              symbol: balance?.symbol,
-              icon_url: '/icons/eth.svg',
-            },
-          } as unknown as TokenBalance,
-        ].sort((a, b) => a.token.symbol.localeCompare(b.value)),
-    })
+        return data as Array<Array<TokenBalance>>
+      } catch (error) {
+        console.error('Error fetching token balances:', error)
+        throw error
+      }
+    },
+    select: (data) => {
+      return [
+        ...(data.flat() ?? []),
+        {
+          value: balance?.value,
+          token: {
+            decimals: balance?.decimals,
+            name: balance?.symbol,
+            symbol: balance?.symbol,
+            icon_url: '/icons/eth.svg',
+          },
+        } as unknown as TokenBalance,
+      ].sort((a, b) => a.token.symbol.localeCompare(b.value))
+    },
+  })
 
   return {
-    data,
+    data: tokenBalances,
     status,
     error,
     refetch,
