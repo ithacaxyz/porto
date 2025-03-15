e8c44c13180a15ca0ca414f3f70033d8d0f19d31
omar <omaraziz.dev@proton.me>
2025-03-14 20:53:56 -0700

    chore: fix create account unalignment

--- app/src/routes/_manager.tsx
+++ app/src/routes/_manager.tsx
@@ -1,7 +1,8 @@
 import { Outlet, createFileRoute } from '@tanstack/react-router'
 import { Toaster } from 'sonner'
 
-import managerCss from '../styles/manager.css?url'
+import { NavigationProgress } from '~/components/NavigationProgress'
+import managerCss from '~/styles/manager.css?url'
 
 export const Route = createFileRoute('/_manager')({
   component: RouteComponent,
@@ -14,6 +15,8 @@ function RouteComponent() {
   return (
     <>
       <Outlet />
+      <NavigationProgress />
+
       <Toaster
         expand={true}
         theme="light"
