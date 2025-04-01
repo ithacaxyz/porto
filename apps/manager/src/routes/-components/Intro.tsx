0244f9a4beacad17c864d543594af90ff04fda4d
omar <omaraziz.dev@proton.me>
2025-04-01 09:42:36 -0700

    fix: types

--- apps/manager/src/routes/-components/Intro.tsx
+++ apps/manager/src/routes/-components/Intro.tsx
@@ -12,7 +12,7 @@ export function Intro() {
           rel="noreferrer"
           target="_blank"
         >
-          <IthacaMono className="w-fit max-w-[75px]" />
+          <IthacaMono />
         </a>
       </p>
       <div className="flex flex-col gap-y-10">
