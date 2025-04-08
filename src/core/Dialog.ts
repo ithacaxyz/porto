4b92545628b3715d9f6da6eabddeb68be51300f7
jshsmth <79778337+jshsmth@users.noreply.github.com>
2025-04-08 21:25:05 +1000

    feat: add contract explorer link

--- src/core/Dialog.ts
+++ src/core/Dialog.ts
@@ -71,7 +71,7 @@ export function iframe() {
       iframe.setAttribute('tabindex', '0')
       iframe.setAttribute(
         'sandbox',
-        'allow-forms allow-scripts allow-same-origin allow-popups',
+        'allow-forms allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox',
       )
       iframe.setAttribute('src', host)
       iframe.setAttribute('title', 'Porto')
@@ -353,7 +353,7 @@ export function experimental_inline(options: inline.Options) {
       iframe.setAttribute('tabindex', '0')
       iframe.setAttribute(
         'sandbox',
-        'allow-scripts allow-same-origin allow-popups',
+        'allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox',
       )
       iframe.setAttribute('src', host)
       iframe.setAttribute('title', 'Porto')
