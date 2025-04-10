c216205f53f707212521c8ec7762aee197ff2f7a
jxom <7336481+jxom@users.noreply.github.com>
2025-04-11 07:07:58 +1000

    chore: up
    Co-authored-by: awkweb <tom@meagher.co>

--- apps/dialog/src/routes/__root.tsx
+++ apps/dialog/src/routes/__root.tsx
@@ -129,7 +129,7 @@ function RouteComponent() {
             className="flex flex-grow *:w-full"
             key={id} // rehydrate on id changes
           >
-            {account.isConnecting ? (
+            {account.isConnecting || account.isReconnecting ? (
               <Layout loading loadingTitle="Loading...">
                 <div />
               </Layout>
