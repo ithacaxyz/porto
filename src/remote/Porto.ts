79d46c98c7dbebb3200205de0708b09923c44247
jxom <7336481+jxom@users.noreply.github.com>
2025-04-10 13:53:33 +1000

    feat: implement relay getCallsStatus

--- src/remote/Porto.ts
+++ src/remote/Porto.ts
@@ -61,6 +61,12 @@ export const defaultConfig = {
       },
     },
     {
+      method: 'wallet_getCallsStatus',
+      modes: {
+        headless: true,
+      },
+    },
+    {
       method: 'wallet_prepareCalls',
       modes: {
         headless: true,
