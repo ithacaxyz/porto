8f019957996d3b04e6250bb7b2f4ba1f36127bfa
jshsmth <79778337+jshsmth@users.noreply.github.com>
2025-04-11 14:15:04 +1000

    feat(ui): create grant admin modal

---
+++ apps/dialog/src/routes/-components/GrantAdmin.tsx
@@ -0,0 +1,74 @@
+import { Button } from '@porto/apps/components'
+import { type RpcSchema } from 'ox'
+import type { RpcSchema as porto_RpcSchema } from 'porto'
+import { Hooks } from 'porto/remote'
+
+import { porto } from '~/lib/Porto'
+import { Layout } from '~/routes/-components/Layout'
+import WalletIcon from '~icons/lucide/wallet-cards'
+import { StringFormatter } from '~/utils'
+
+export function GrantAdmin(props: GrantAdmin.Props) {
+  const { address, loading, onApprove, onReject } = props
+
+  const account = Hooks.useAccount(porto, { address })
+
+  return (
+    <Layout loading={loading} loadingTitle="Authorizing...">
+      <Layout.Header>
+        <Layout.Header.Default
+          content={
+            <div>You will allow this account to recover your passkey if it is ever lost.</div>
+          }
+          title="Add backup method"
+        />
+      </Layout.Header>
+      <Layout.Content>
+        <div className="bg-gray-100 rounded-md p-2 flex items-center justify-center">
+          {account?.address && (
+            <div className="flex items-center gap-2">
+              <div className="flex h-6 w-6 items-center justify-center rounded-full bg-jade4 shrink-0">
+                <WalletIcon className="h-4 w-4 text-jade9" />
+              </div>
+              <span className="font-mono text-base font-semibold">
+                {StringFormatter.truncate(account.address)}
+              </span>
+            </div>
+          )}
+        </div>
+      </Layout.Content>
+
+      <Layout.Footer>
+        <Layout.Footer.Actions>
+          <Button className="flex-1" onClick={onReject} type="button">
+            Cancel
+          </Button>
+
+          <Button
+            className="flex-1"
+            onClick={onApprove}
+            type="button"
+            variant="accent"
+          >
+            Add
+          </Button>
+        </Layout.Footer.Actions>
+
+        {account?.address && (
+          <Layout.Footer.Account address={account.address} />
+        )}
+      </Layout.Footer>
+    </Layout>
+  )
+}
+
+export declare namespace GrantAdmin {
+  type Props = RpcSchema.ExtractParams<
+    porto_RpcSchema.Schema,
+    'experimental_grantAdmin'
+  >['0'] & {
+    loading: boolean
+    onApprove: () => void
+    onReject: () => void
+  }
+}
