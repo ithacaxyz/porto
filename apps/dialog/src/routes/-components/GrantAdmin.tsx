d56dc25bbf7f5d7250a97db964bb292d259e1173
jshsmth <79778337+jshsmth@users.noreply.github.com>
2025-04-14 14:24:34 +1000

    feat: add error handling for status error

--- apps/dialog/src/routes/-components/GrantAdmin.tsx
+++ apps/dialog/src/routes/-components/GrantAdmin.tsx
@@ -6,14 +6,14 @@ import type * as Address from 'ox/Address'
 import { Key, Relay } from 'porto/internal'
 import { Hooks } from 'porto/remote'
 import * as React from 'react'
+import * as Dialog from '~/lib/Dialog'
 import { porto } from '~/lib/Porto'
 import * as Price from '~/lib/Price'
 import { Layout } from '~/routes/-components/Layout'
 import { StringFormatter } from '~/utils'
-import WalletIcon from '~icons/lucide/wallet-cards'
 import TriangleAlert from '~icons/lucide/triangle-alert'
+import WalletIcon from '~icons/lucide/wallet-cards'
 import { useQuote } from './ActionRequest'
-import * as Dialog from '~/lib/Dialog'
 
 export function GrantAdmin(props: GrantAdmin.Props) {
   const {
