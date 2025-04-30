74a61c6ec0458f4dccaead0055c82c05add1929a
jxom <7336481+jxom@users.noreply.github.com>
2025-05-01 06:42:56 +1000

    up

--- scripts/build:anvil-state.ts
+++ scripts/build:anvil-state.ts
@@ -97,7 +97,7 @@ let entryPointAddress: Address.Address | null | undefined
 for (const i of Array.from({ length: 2 }, (_, i) => i + 1)) {
   const hash = await deployContract(client, {
     abi: ExperimentERC20.abi,
-    args: [`Exp${i}`, `EXP${i}`, i === 0 ? 100n : 1n],
+    args: ['ExperimentERC20', 'EXP', i === 0 ? 100n : 1n],
     bytecode: ExperimentERC20.code,
     chain: null,
   })
