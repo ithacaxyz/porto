3e21ca85f73c9cbb129aa95af195394973e7cf81
jxom <7336481+jxom@users.noreply.github.com>
2025-04-30 10:23:51 +1000

    feat: add `storageKey`

--- apps/dialog/src/lib/Porto.ts
+++ apps/dialog/src/lib/Porto.ts
@@ -2,6 +2,7 @@ import { PortoConfig } from '@porto/apps'
 import { Storage } from 'porto'
 import { Porto } from 'porto/remote'
 
+console.log(PortoConfig.getConfig())
 export const porto = Porto.create({
   ...PortoConfig.getConfig(),
   storage: Storage.combine(Storage.cookie(), Storage.localStorage()),
