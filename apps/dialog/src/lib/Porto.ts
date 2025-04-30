9e3c1d66b308f9a433bb522709e8014c13ef41cb
jxom <7336481+jxom@users.noreply.github.com>
2025-04-30 10:24:55 +1000

    Update Porto.ts

--- apps/dialog/src/lib/Porto.ts
+++ apps/dialog/src/lib/Porto.ts
@@ -2,7 +2,6 @@ import { PortoConfig } from '@porto/apps'
 import { Storage } from 'porto'
 import { Porto } from 'porto/remote'
 
-console.log(PortoConfig.getConfig())
 export const porto = Porto.create({
   ...PortoConfig.getConfig(),
   storage: Storage.combine(Storage.cookie(), Storage.localStorage()),
