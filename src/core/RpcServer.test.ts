19c6caf8bc95822be5c59ddd96d390500f819644
jxom <7336481+jxom@users.noreply.github.com>
2025-05-21 09:00:35 +0200

    tests: add

--- src/core/RpcServer.test.ts
+++ src/core/RpcServer.test.ts
@@ -233,53 +233,7 @@ describe('sendCalls', () => {
     ).toBe(100n)
   })
 
-  test('behavior: via prepareCalls', async () => {
-    const key = Key.createHeadlessWebAuthnP256()
-    const account = await TestActions.createAccount(client, {
-      keys: [key],
-    })
-
-    const request = await Rpc.prepareCalls(client, {
-      account,
-      calls: [
-        {
-          abi: exp2Abi,
-          args: [account.address, 100n],
-          functionName: 'mint',
-          to: exp2Address,
-        },
-      ],
-      feeToken: exp1Address,
-      key,
-    })
-
-    const signature = await Key.sign(key, {
-      payload: request.digest,
-      wrap: false,
-    })
-
-    const { id } = await Rpc.sendCalls(client, {
-      ...request,
-      key: request.key!,
-      signature,
-    })
-
-    expect(id).toBeDefined()
-
-    await waitForCallsStatus(client, {
-      id,
-    })
-
-    expect(
-      await readContract(client, {
-        ...exp2Config,
-        args: [account.address],
-        functionName: 'balanceOf',
-      }),
-    ).toBe(100n)
-  })
-
-  test('behavior: pre bundles', async () => {
+  test('behavior: pre calls', async () => {
     const key = Key.createHeadlessWebAuthnP256()
     const account = await TestActions.createAccount(client, {
       deploy: true,
@@ -335,7 +289,116 @@ describe('sendCalls', () => {
     ).toBe(100n)
   })
 
-  test('behavior: pre bundles (via prepareCalls)', async () => {
+  test('behavior: pre calls; authorize session key, sign with session key', async () => {
+    const adminKey = Key.createHeadlessWebAuthnP256()
+    const account = await TestActions.createAccount(client, {
+      keys: [adminKey],
+    })
+
+    const sessionKey = Key.createP256({
+      expiry: 9999999999,
+      permissions: {
+        calls: [{ to: exp2Address }],
+        spend: [
+          {
+            limit: Value.fromEther('5'),
+            period: 'minute',
+            token: exp1Address,
+          },
+        ],
+      },
+      role: 'session',
+    })
+
+    const request_1 = await Rpc.prepareCalls(client, {
+      authorizeKeys: [sessionKey],
+      feeToken: exp1Address,
+      preCalls: true,
+    })
+    const signature_1 = await Key.sign(adminKey, {
+      payload: request_1.digest,
+      wrap: false,
+    })
+
+    const { id } = await Rpc.sendCalls(client, {
+      account,
+      calls: [
+        {
+          abi: exp2Abi,
+          args: [account.address, 100n],
+          functionName: 'mint',
+          to: exp2Address,
+        },
+      ],
+      feeToken: exp1Address,
+      key: sessionKey,
+      preCalls: [{ ...(request_1 as any), signature: signature_1 }],
+    })
+
+    expect(id).toBeDefined()
+
+    await waitForCallsStatus(client, {
+      id,
+    })
+
+    expect(
+      await readContract(client, {
+        ...exp2Config,
+        args: [account.address],
+        functionName: 'balanceOf',
+      }),
+    ).toBe(100n)
+  })
+})
+
+describe('prepareCalls', () => {
+  test('default', async () => {
+    const key = Key.createHeadlessWebAuthnP256()
+    const account = await TestActions.createAccount(client, {
+      keys: [key],
+    })
+
+    const request = await Rpc.prepareCalls(client, {
+      account,
+      calls: [
+        {
+          abi: exp2Abi,
+          args: [account.address, 100n],
+          functionName: 'mint',
+          to: exp2Address,
+        },
+      ],
+      feeToken: exp1Address,
+      key,
+    })
+
+    const signature = await Key.sign(key, {
+      payload: request.digest,
+      wrap: false,
+    })
+
+    const { id } = await Rpc.sendCalls(client, {
+      ...request,
+      key: request.key!,
+      signature,
+    })
+
+    expect(id).toBeDefined()
+
+    await waitForCallsStatus(client, {
+      id,
+    })
+
+    expect(
+      await readContract(client, {
+        ...exp2Config,
+        args: [account.address],
+        functionName: 'balanceOf',
+      }),
+    ).toBe(100n)
+  })
+
+  test('behavior: pre calls', async () => {
     const key = Key.createHeadlessWebAuthnP256()
     const account = await TestActions.createAccount(client, {
       keys: [key],
@@ -405,7 +468,7 @@ describe('sendCalls', () => {
     ).toBe(100n)
   })
 
-  test('behavior: pre bundles (via prepareCalls); account has executed calls previously', async () => {
+  test('behavior: pre calls; account has executed calls previously', async () => {
     const key = Key.createHeadlessWebAuthnP256()
     const account = await TestActions.createAccount(client, {
       keys: [key],
@@ -456,6 +519,77 @@ describe('sendCalls', () => {
       preCalls: [{ ...request_1, signature: signature_1 }],
     })
     const signature_2 = await Key.sign(key, {
+      payload: request_2.digest,
+      wrap: false,
+    })
+
+    const { id } = await Rpc.sendCalls(client, {
+      ...request_2,
+      key: request_2.key!,
+      signature: signature_2,
+    })
+
+    expect(id).toBeDefined()
+
+    await waitForCallsStatus(client, {
+      id,
+    })
+
+    expect(
+      await readContract(client, {
+        ...exp2Config,
+        args: [account.address],
+        functionName: 'balanceOf',
+      }),
+    ).toBe(100n)
+  })
+
+  test('behavior: pre calls; authorize session key, sign with session key', async () => {
+    const adminKey = Key.createHeadlessWebAuthnP256()
+    const account = await TestActions.createAccount(client, {
+      keys: [adminKey],
+    })
+
+    const sessionKey = Key.createP256({
+      expiry: 9999999999,
+      permissions: {
+        calls: [{ to: exp2Address }],
+        spend: [
+          {
+            limit: Value.fromEther('5'),
+            period: 'minute',
+            token: exp1Address,
+          },
+        ],
+      },
+      role: 'session',
+    })
+
+    const request_1 = await Rpc.prepareCalls(client, {
+      authorizeKeys: [sessionKey],
+      feeToken: exp1Address,
+      preCalls: true,
+    })
+    const signature_1 = await Key.sign(adminKey, {
+      payload: request_1.digest,
+      wrap: false,
+    })
+
+    const request_2 = await Rpc.prepareCalls(client, {
+      account,
+      calls: [
+        {
+          abi: exp2Abi,
+          args: [account.address, 100n],
+          functionName: 'mint',
+          to: exp2Address,
+        },
+      ],
+      feeToken: exp1Address,
+      key: sessionKey,
+      preCalls: [{ ...request_1, signature: signature_1 }],
+    })
+    const signature_2 = await Key.sign(sessionKey, {
       payload: request_2.digest,
       wrap: false,
     })
