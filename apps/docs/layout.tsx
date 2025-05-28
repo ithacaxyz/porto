c07af11dc3d8fadedf67db2257d06853905c5eb3
Achal Srinivasan <achal@achal.me>
2025-05-27 23:41:41 -0700

    Fix erroneous import

--- apps/docs/layout.tsx
+++ apps/docs/layout.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
-import Head from 'next/head'
 
 import { Connect } from './components/Connect'
 import * as WagmiConfig from './wagmi.config'
@@ -9,17 +8,11 @@ const queryClient = new QueryClient()
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <Head>
-        <meta property="og:image" content="/og-image.png" />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-      </Head>
-      <WagmiProvider config={WagmiConfig.config}>
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-      </WagmiProvider>
-    </>
+    <WagmiProvider config={WagmiConfig.config}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 }
 
