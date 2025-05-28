ddd1777ec260b2af37ac243eea43affa3e078365
Achal Srinivasan <achal@achal.me>
2025-05-27 23:38:34 -0700

    Add OpenGraph image

--- apps/docs/layout.tsx
+++ apps/docs/layout.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
+import Head from 'next/head'
 
 import { Connect } from './components/Connect'
 import * as WagmiConfig from './wagmi.config'
@@ -8,9 +9,17 @@ const queryClient = new QueryClient()
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider config={WagmiConfig.config}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </WagmiProvider>
+    <>
+      <Head>
+        <meta property="og:image" content="/og-image.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
+      <WagmiProvider config={WagmiConfig.config}>
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </WagmiProvider>
+    </>
   )
 }
 
