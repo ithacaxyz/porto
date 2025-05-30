31760b443dcc7aeba7418537ca1c7a6fb637f169
jxom <7336481+jxom@users.noreply.github.com>
2025-05-30 18:22:39 +0200

    up

--- src/viem/index.ts
+++ src/viem/index.ts
@@ -1,3 +1,4 @@
 export type { CapabilitiesSchema } from './CapabilitiesSchema.js'
 export type { RpcSchema } from './RpcSchema.js'
 export * as ServerActions from './ServerActions.js'
+export { decorator as serverActions } from './ServerActions.js'
