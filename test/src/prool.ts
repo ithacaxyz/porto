4cdfec8ac4566297bb034533bd14faef481b2735
jxom <7336481+jxom@users.noreply.github.com>
2025-06-04 10:17:27 +0200

    wip: checkpoint

--- test/src/prool.ts
+++ test/src/prool.ts
@@ -159,7 +159,8 @@ export const rpcServer = defineInstance((parameters?: RpcServerParameters) => {
             if (
               body.method &&
               (body.method.startsWith('relay_') ||
-                body.method.startsWith('wallet_'))
+                body.method.startsWith('wallet_') ||
+                body.method === 'health')
             )
               return `http://${host}:${port_relay}`
             return endpoint
