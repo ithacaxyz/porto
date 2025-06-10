509bf4bb5036844aea2dd753417c0a1b0b94fb02
o-az <omaraziz.dev@proton.me>
2025-06-10 08:51:29 -0700

    fix: wrap component content instead of parent

--- apps/dialog/src/routes/dialog/wallet_revokePermissions.tsx
+++ apps/dialog/src/routes/dialog/wallet_revokePermissions.tsx
@@ -1,12 +1,9 @@
 import { useMutation } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
-import { Key } from 'porto'
-import { Actions, Hooks } from 'porto/remote'
+import { Actions } from 'porto/remote'
 
-import { CheckBalance } from '~/components/CheckBalance'
 import { porto } from '~/lib/Porto'
 import * as Router from '~/lib/Router'
-import * as RpcServer from '~/lib/RpcServer'
 import { RevokePermissions } from '../-components/RevokePermissions'
 
 export const Route = createFileRoute('/dialog/wallet_revokePermissions')({
@@ -22,16 +19,6 @@ function RouteComponent() {
   const request = Route.useSearch()
   const parameters = request.params[0]
 
-  const { data } = Hooks.usePermissions()
-  const permissions = data?.find((x) => x.id === parameters.id)?.permissions
-  const revokeKey = permissions?.keys?.find((key) => key.id === parameters.id)
-
-  const prepareCallsQuery = RpcServer.usePrepareCalls({
-    enabled: !!revokeKey,
-    feeToken: parameters.capabilities?.feeToken,
-    revokeKeys: revokeKey ? [Key.from(revokeKey)] : [],
-  })
-
   const respond = useMutation({
     mutationFn() {
       return Actions.respond(porto, request)
@@ -39,18 +26,11 @@ function RouteComponent() {
   })
 
   return (
-    <CheckBalance
-      address={parameters.address}
-      feeToken={parameters.capabilities?.feeToken}
+    <RevokePermissions
+      {...parameters}
+      loading={respond.isPending}
+      onApprove={() => respond.mutate()}
       onReject={() => Actions.reject(porto, request)}
-      query={prepareCallsQuery}
-    >
-      <RevokePermissions
-        {...parameters}
-        loading={respond.isPending}
-        onApprove={() => respond.mutate()}
-        onReject={() => Actions.reject(porto, request)}
-      />
-    </CheckBalance>
+    />
   )
 }
