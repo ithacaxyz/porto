7cc2278fabaa38da512dd37cb8ef037d9184807a
o-az <omaraziz.dev@proton.me>
2025-06-12 02:23:48 -0700

    chore: refine webauthn credential creation params

--- src/viem/Key.ts
+++ src/viem/Key.ts
@@ -241,11 +241,15 @@ export async function createWebAuthnP256(
 
   const credential = await WebAuthnP256.createCredential({
     authenticatorSelection: {
-      requireResidentKey: false,
+      authenticatorAttachment: 'platform',
+      requireResidentKey: true,
       residentKey: 'preferred',
       userVerification: 'required',
     },
     createFn,
+    extensions: {
+      credProps: true,
+    },
     rp: rpId
       ? {
           id: rpId,
@@ -892,7 +896,7 @@ export async function sign(
 
       const cacheKey = `porto.webauthnVerified.${key.hash}`
       const now = Date.now()
-      const verificationTimeout = 10 * 60 * 1000 // 10 minutes in milliseconds
+      const verificationTimeout = 10 * 60 * 1_000 // 10 minutes in milliseconds
 
       let requireVerification = true
       if (storage) {
@@ -913,10 +917,15 @@ export async function sign(
       })
 
       const response = raw.response as AuthenticatorAssertionResponse
+      if (!response?.userHandle)
+        throw new Error('No user handle in response', {
+          cause: { response },
+        })
       const id = Bytes.toHex(new Uint8Array(response.userHandle!))
       if (key.id && !Address.isEqual(key.id, id))
         throw new Error(
           `supplied webauthn key "${key.id}" does not match signature webauthn key "${id}"`,
+          { cause: { id, key } },
         )
 
       if (requireVerification && storage) await storage.setItem(cacheKey, now)
