221408277e53468070dbcbc27f4932420c5903b9
jxom <7336481+jxom@users.noreply.github.com>
2025-06-17 09:22:00 +0200

    up

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
-import * as prompts from '@clack/prompts'
 import { cac } from 'cac'
-import { createClient, http } from 'viem'
-import { getChainId } from 'viem/actions'
 
-import * as Chains from '../core/Chains.js'
 import pkgJson from '../package.json' with { type: 'json' }
-import * as Key from '../viem/Key.js'
-import * as ServerActions from '../viem/ServerActions.js'
+import * as Commands from './internal/commands.js'
+import * as Utils from './internal/utils.js'
 
 const cli = cac('porto')
 
@@ -28,93 +24,3 @@ cli.help()
 cli.version(pkgJson.version)
 
 cli.parse()
-
-namespace Commands {
-  /** Creates a Porto account. */
-  export async function createAccount(
-    _: unknown,
-    args: createAccount.Arguments,
-  ) {
-    const client = await Context.getClient(args)
-
-    prompts.intro('Create Account')
-
-    const key = Key.createSecp256k1()
-
-    const s = prompts.spinner()
-    s.start('Creating...')
-    const account = await ServerActions.createAccount(client, {
-      authorizeKeys: [key],
-    }).catch((error) => {
-      s.stop('Failed. ')
-      throw error
-    })
-    s.stop('Created. ')
-
-    prompts.log.info('Address:     ' + account.address)
-    prompts.log.info('Private key: ' + key.privateKey!()!)
-  }
-
-  export declare namespace createAccount {
-    type Arguments = {
-      /** Chain name. */
-      chain?: string | undefined
-      /** RPC Server URL. */
-      rpc?: string | undefined
-    }
-  }
-}
-
-namespace Context {
-  /** Gets a Viem client for RPC Server. */
-  export async function getClient(options: getPorto.Options = {}) {
-    const chain = Utils.kebabToCamel(options.chain!) as keyof typeof Chains
-    const client = createClient({
-      chain: Chains[chain] as Chains.Chain,
-      transport: http(options.rpc),
-    })
-    client.chain = {
-      ...client.chain,
-      id: (await getChainId(client)) as never,
-    }
-    return client
-  }
-
-  export declare namespace getPorto {
-    type Options = {
-      /** Chain name. */
-      chain?: string | undefined
-      /** RPC Server URL. */
-      rpc?: string | undefined
-    }
-  }
-}
-
-namespace Utils {
-  /** Gets all chain names. */
-  export function getChainNames() {
-    return Object.entries(Chains)
-      .filter(([_, chain]) => typeof chain === 'object' && 'id' in chain)
-      .map(([key]) => camelToKebab(key))
-  }
-
-  /** Converts kebab-case string to camelCase. */
-  export function kebabToCamel(str: string): string {
-    return str
-      .split('-')
-      .map((word, index) =>
-        index === 0
-          ? word.toLowerCase()
-          : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
-      )
-      .join('')
-  }
-
-  /** Converts camelCase string to kebab-case. */
-  export function camelToKebab(str: string): string {
-    return str
-      .split(/(?=[A-Z])/)
-      .map((word) => word.toLowerCase())
-      .join('-')
-  }
-}
