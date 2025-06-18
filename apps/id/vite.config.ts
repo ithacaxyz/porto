6d04ae89338a0e904ab5c48e4cac87b616d08956
jxom <7336481+jxom@users.noreply.github.com>
2025-06-18 07:35:06 +0200

    Update vite.config.ts

--- apps/id/vite.config.ts
+++ apps/id/vite.config.ts
@@ -17,7 +17,7 @@ const commitSha =
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const skipMkcert = env.SKIP_MKCERT === 'true' || mode === 'test'
+  const skipMkcert = env.SKIP_MKCERT === 'true'
 
   // don't index id.porto.sh except in production
   if (env.NODE_ENV === 'production' && env.VITE_VERCEL_ENV === 'production') {
