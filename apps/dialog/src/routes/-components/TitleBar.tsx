27ce7122473fc53f5c12d689573f922166622c00
jxom <7336481+jxom@users.noreply.github.com>
2025-06-20 11:03:33 +0200

    up

--- apps/dialog/src/routes/-components/TitleBar.tsx
+++ apps/dialog/src/routes/-components/TitleBar.tsx
@@ -102,7 +102,7 @@ export function TitleBar(props: TitleBar.Props) {
         )}
       </div>
 
-      {mode !== 'inline-iframe' && mode !== 'popup-standalone' && (
+      {mode !== 'inline-iframe' && mode !== 'standalone' && (
         <button
           onClick={() => Actions.rejectAll(porto)}
           title="Close Dialog"
