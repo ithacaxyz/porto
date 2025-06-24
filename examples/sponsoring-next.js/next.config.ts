0f73aedd4028669dbde3f5c0bcec3ff1fcf01784
jxom <7336481+jxom@users.noreply.github.com>
2025-06-24 19:17:39 +0200

    up

--- examples/sponsoring-next.js/next.config.ts
+++ examples/sponsoring-next.js/next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from 'next'
 
 export default {
-  experimental: {
-    externalDir: true,
-  },
   reactStrictMode: true,
-  webpack(config) {
-    config.resolve.extensionAlias = {
-      '.js': ['.js', '.ts'],
-    }
-    return config
-  },
 } satisfies NextConfig
