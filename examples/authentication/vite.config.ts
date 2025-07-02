feb137e8ac2c2830b0fa866211ade80a4cbadc93
jxom <7336481+jxom@users.noreply.github.com>
2025-07-02 15:03:45 +0100

    u

--- examples/authentication/vite.config.ts
+++ examples/authentication/vite.config.ts
@@ -1,11 +1,10 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare(), mkcert()],
+  plugins: [react(), cloudflare()],
   server: {
     cors: {
       origin: '*',
