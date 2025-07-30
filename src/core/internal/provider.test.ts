4bad1009746678ed1c8f6626214fc2378a3531e0
o-az <omaraziz.dev@proton.me>
2025-07-29 23:19:11 -0700

    chore: sync tests

--- src/core/internal/provider.test.ts
+++ src/core/internal/provider.test.ts
@@ -3015,6 +3015,58 @@ describe.each([
     })
   })
 
+  describe('wallet_getCallsStatus', () => {
+    test('default', async () => {
+      const porto = getPorto()
+      const client = TestConfig.getServerClient(porto)
+      const contracts = TestConfig.getContracts(porto)
+
+      const {
+        accounts: [account],
+      } = await porto.provider.request({
+        method: 'wallet_connect',
+        params: [{ capabilities: { createAccount: true } }],
+      })
+      const address = account!.address
+
+      await setBalance(client, {
+        address,
+        value: Value.fromEther('10000'),
+      })
+
+      const alice = Hex.random(20)
+
+      const { id } = await porto.provider.request({
+        method: 'wallet_sendCalls',
+        params: [
+          {
+            calls: [
+              {
+                data: encodeFunctionData({
+                  abi: contracts.exp1.abi,
+                  args: [alice, 69420n],
+                  functionName: 'transfer',
+                }),
+                to: contracts.exp1.address,
+              },
+            ],
+            from: address,
+            version: '1',
+          },
+        ],
+      })
+
+      expect(id).toBeDefined()
+
+      const response = await porto.provider.request({
+        method: 'wallet_getCallsStatus',
+        params: [id],
+      })
+
+      expect(response.id).toBe(id)
+    })
+  })
+
   describe('wallet_prepareCalls → wallet_sendPreparedCalls', () => {
     describe('behavior: permissions', () => {
       test('default', async () => {
