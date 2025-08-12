87f9d5fb6b511da1e2946c1a1982e9ac0abaa086
o-az <omaraziz.dev@proton.me>
2025-08-11 23:05:35 -0700

    temporarily move widget script to index.html

--- apps/dialog/src/routes/dialog/wallet_addFunds.tsx
+++ apps/dialog/src/routes/dialog/wallet_addFunds.tsx
@@ -6,9 +6,9 @@ import { AddFunds } from '../-components/AddFunds'
 
 export const Route = createFileRoute('/dialog/wallet_addFunds')({
   component: RouteComponent,
-  head: (_ctx) => ({
-    scripts: [{ src: 'https://widget.mercuryo.io/embed.2.1.js' }],
-  }),
+  // head: (_ctx) => ({
+  //   scripts: [{ src: 'https://widget.mercuryo.io/embed.2.1.js' }],
+  // }),
   validateSearch(search) {
     return Router.parseSearchRequest(search, {
       method: 'wallet_addFunds',
@@ -18,15 +18,12 @@ export const Route = createFileRoute('/dialog/wallet_addFunds')({
 
 function RouteComponent() {
   const request = Route.useSearch()
-  // @ts-expect-error - TODO: fix this
   const { address, token, value } = request._decoded.params[0]
 
   return (
     <AddFunds
       address={address}
-      // @ts-expect-error - TODO: fix this
       onApprove={(result) => Actions.respond(porto, request!, { result })}
-      // @ts-expect-error - TODO: fix this
       onReject={() => Actions.reject(porto, request)}
       tokenAddress={token}
       value={value}
