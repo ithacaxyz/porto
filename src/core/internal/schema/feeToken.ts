43faff70d8a83b10ee3605f364ede8299d26e6a3
Tom Meagher <tom@meagher.co>
2025-08-14 11:45:37 -0400

    refactor: remove FeeToken.kind schema

--- src/core/internal/schema/feeToken.ts
+++ src/core/internal/schema/feeToken.ts
@@ -11,13 +11,5 @@ export const FeeToken = Schema.Struct({
 })
 export type FeeToken = typeof FeeToken.Type
 
-// TODO: remove
-export const Kind = Schema.Union(
-  Schema.Literal('ETH'),
-  Schema.Literal('USDC'),
-  Schema.Literal('USDT'),
-)
-export type Kind = typeof Kind.Type
-
 export const Symbol = Schema.String
 export type Symbol = typeof Symbol.Type
