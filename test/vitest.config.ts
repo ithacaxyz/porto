b62bc500208496c13a925678afe8d4ada243c0d4
jxom <7336481+jxom@users.noreply.github.com>
2025-08-17 17:42:16 +0200

    feat: rename RPC Server terminology to Relay
    BREAKING CHANGE: Renamed "RPC Server"/"Server" terminology to "Relay".
    
    - Renamed Mode.rpcServer() to Mode.relay()
    - Renamed ServerActions to RelayActions
    - Renamed ServerClient to RelayClient
    - Updated all imports and references throughout codebase
    - Moved rpcServer directories to relay directories
    - Updated documentation to reflect new terminology
    
    Migration:
    ```diff
    import {
      Mode,
    - ServerActions,
    - ServerClient,
    + RelayActions,
    + RelayClient,
    } from 'porto'
    
    - Mode.rpcServer(...)
    + Mode.relay(...)
    ```

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -35,9 +35,8 @@ export default defineConfig(({ mode }) => {
           test: {
             globalSetup: [join(__dirname, './globalSetup.ts')],
             hookTimeout: 20_000,
+            exclude: ['**/*.yaml', 'src/**/*.browser.test.ts'],
             include: [
-              '!*.yaml',
-              '!src/**/*.browser.test.ts',
               'src/**/*.test.ts',
               ...(env.VITE_DEFAULT_ENV !== 'anvil'
                 ? ['!src/**/*ContractActions.test.ts']
