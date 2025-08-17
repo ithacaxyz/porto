475e531d3e9aeebcb0d21526f5e2e0aa76ff0f27
o-az <omaraziz.dev@proton.me>
2025-08-16 22:50:24 -0700

    feat: use tanstack start for id

---
+++ apps/new-id/src/lib/Porto.ts
@@ -0,0 +1,19 @@
+import { Env, PortoConfig } from '@porto/apps'
+import { Mode, type Porto, Storage } from 'porto'
+
+const host = (() => {
+  const url = new URL(PortoConfig.getDialogHost())
+  if (import.meta.env.DEV) url.port = window.location.port
+  return url.href
+})()
+
+export const config = {
+  ...PortoConfig.getConfig(),
+  experimental: {
+    applePayOnramp: Env.get() === 'prod',
+  },
+  mode: Mode.dialog({
+    host,
+  }),
+  storage: Storage.combine(Storage.cookie(), Storage.localStorage()),
+} as const satisfies Porto.Config
