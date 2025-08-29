ca0210782aaf23b90a76a1670c9679723fa0135a
o-az <omaraziz.dev@proton.me>
2025-08-28 19:23:40 -0700

    chore: reliable solution

--- apps/id/src/lib/Wagmi.ts
+++ apps/id/src/lib/Wagmi.ts
@@ -1,9 +1,29 @@
+import { Chains } from 'porto'
 import { porto as portoConnector } from 'porto/wagmi'
+import type { Chain } from 'viem'
 import { createConfig, createStorage } from 'wagmi'
+import { create } from 'zustand'
 import * as Porto from './Porto'
 
-export const config = createConfig({
+// A chains store that we can update in special cases (eg. in recovery)
+export const useChainsStore = create<{
+  chains: readonly [Chain, ...Chain[]]
+  addChain: (chain: Chain) => void
+  removeChain: (chain: Chain) => void
+  setChains: (chains: Chain[]) => void
+}>()((set) => ({
+  addChain: (chain: Chain) =>
+    set((state) => ({ chains: [...state.chains, chain] })),
   chains: Porto.config.chains,
+  removeChain: (chain: Chain) =>
+    set((state) => ({
+      chains: state.chains.filter((c) => c.id !== chain.id) as never,
+    })),
+  setChains: (chains: Chain[]) => set({ chains: chains as never }),
+}))
+
+export const config = createConfig({
+  chains: useChainsStore.getState().chains,
   connectors: [portoConnector(Porto.config)],
   multiInjectedProviderDiscovery: false,
   storage: createStorage({ storage: localStorage }),
@@ -11,10 +31,13 @@ export const config = createConfig({
 })
 
 export const mipdConfig = createConfig({
-  chains: Porto.config.chains,
+  chains: [Chains.base, Chains.baseSepolia],
   multiInjectedProviderDiscovery: true,
   storage: null,
-  transports: config._internal.transports,
+  transports: {
+    [Chains.base.id]: config._internal.transports[Chains.base.id],
+    [Chains.baseSepolia.id]: config._internal.transports[Chains.baseSepolia.id],
+  },
 })
 
 export const getChainConfig = (chainId: number) =>
--- apps/id/src/routes/_dash/settings/recovery.tsx
+++ apps/id/src/routes/_dash/settings/recovery.tsx
@@ -1,56 +1,80 @@
 import * as Ariakit from '@ariakit/react'
+import { Toast } from '@porto/apps/components'
 import {
   CatchBoundary,
   createFileRoute,
   Link,
-  type RouterEvents,
   useRouteContext,
-  useRouter,
 } from '@tanstack/react-router'
+import { Chains } from 'porto'
 import { Hooks } from 'porto/wagmi'
 import * as React from 'react'
+import { toast } from 'sonner'
 import { baseSepolia } from 'viem/chains'
 import {
-  useAccount,
-  useAccountEffect,
+  type Connector,
   useConnect,
   useConnectors,
   useDisconnect,
   useSwitchChain,
 } from 'wagmi'
-import { mipdConfig } from '~/lib/Wagmi'
+import { useRouterEvent } from '~/hooks/useRouterEvents'
+import { config, mipdConfig, useChainsStore } from '~/lib/Wagmi'
 
 export const Route = createFileRoute('/_dash/settings/recovery')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const [view, setView] = React.useState<
-    'default' | 'success' | 'loading' | 'error'
-  >('default')
-
-  const { account: portoAccount } = useRouteContext({
+  const { account } = useRouteContext({
     from: '/_dash/settings/recovery',
   })
-  const chainId = window.location.search.includes('testnet')
-    ? baseSepolia.id
-    : (portoAccount?.chainId as never)
+  const [_view, setView] = React.useState<'default' | 'success' | 'loading'>(
+    'default',
+  )
 
-  const grantAdmin = Hooks.useGrantAdmin()
+  const chainStore = useChainsStore()
+  const testnet = window.location.search.includes('testnet')
+  const chainId = testnet ? baseSepolia.id : (account.chainId as never)
 
-  const account = useAccount({ config: mipdConfig })
-  const connect = useConnect({
-    config: mipdConfig,
-    mutation: {
-      onMutate: (_variables) => {
-        if (account.chainId === chainId) return
-        switchChain.switchChain({ chainId })
-      },
-    },
+  useRouterEvent('onBeforeNavigate', (event) => {
+    if (event.hrefChanged && event.fromLocation)
+      chainStore.setChains(config.chains as never)
   })
-  const connectors = useConnectors({ config: mipdConfig })
+
+  useRouterEvent('onResolved', () => {
+    disconnectAll()
+    chainStore.setChains(testnet ? [Chains.baseSepolia] : [Chains.base])
+  })
+
+  const connect = useConnect({ config: mipdConfig })
   const disconnect = useDisconnect({ config: mipdConfig })
   const switchChain = useSwitchChain({ config: mipdConfig })
+  const connectors = useConnectors({ config: mipdConfig }).filter(
+    (c) => !c.id.toLowerCase().includes('porto'),
+  )
+
+  const grantAdmin = Hooks.useGrantAdmin()
+  const { data: admins } = Hooks.useAdmins({
+    query: {
+      select: (data) => ({
+        ...data,
+        keys: data.keys.filter((key) => key.type !== 'address'),
+      }),
+    },
+  })
+
+  async function tryConnect(connector: Connector) {
+    try {
+      const {
+        accounts: [address],
+      } = await connect.connectAsync({ chainId, connector })
+      return address
+    } catch {
+      await disconnect.disconnectAsync()
+      return undefined
+    }
+  }
 
   const disconnectAll = async () =>
     Promise.all([
@@ -58,18 +82,61 @@ function RouteComponent() {
       ...connectors.map((connector) => connector.disconnect()),
     ])
 
-  useRouterEvent('onBeforeNavigate', (event) => {
-    if (event.hrefChanged && event.fromLocation) disconnectAll()
-  })
+  const connectThenGrantAdmin = async (
+    event: React.MouseEvent<HTMLButtonElement>,
+    connector: Connector,
+  ) => {
+    event.preventDefault()
+    event.stopPropagation()
 
-  useRouterEvent('onResolved', () => switchChain.switchChain({ chainId }))
+    try {
+      // 1. disconnect in case user is connected from previous sessions
+      await disconnectAll()
 
-  useAccountEffect({
-    onConnect: (data) => {
-      if (data.chainId === chainId) return
-      switchChain.switchChain({ chainId })
-    },
-  })
+      // 2. try to connect -- this could fail for a number of reasons:
+      // - one of which is the user doesn't have the chain configured
+      let address = await tryConnect(connector)
+      if (!address) {
+        await switchChain.switchChainAsync({
+          chainId,
+        })
+        address = await tryConnect(connector)
+      }
+
+      if (!address) throw new Error('Failed to connect to wallet')
+
+      const granted = await grantAdmin.mutateAsync({
+        address: account.address,
+        chainId,
+        key: { publicKey: address, type: 'address' },
+      })
+
+      if (!granted) throw new Error('Failed to grant admin permissions')
+
+      setView('success')
+      await disconnectAll()
+    } catch (error) {
+      await disconnectAll()
+      console.info(error)
+      let message = 'Encountered an error while granting admin permissions.'
+      if (
+        error instanceof Error &&
+        error.message.includes('Key already granted')
+      ) {
+        message = 'Key already granted as admin'
+      }
+      toast.custom((t) => (
+        <Toast
+          className={t}
+          description={message}
+          kind="warn"
+          title="Did not go through"
+        />
+      ))
+    } finally {
+      await disconnectAll()
+    }
+  }
 
   if (!connectors.length) return <div>No wallets found</div>
   return (
@@ -80,45 +147,20 @@ function RouteComponent() {
         disconnectAll()
       }}
     >
-      <div>
+      <section className="flex flex-col gap-4">
         <h2>Add recovery wallet</h2>
         <p>
           If you lose access, recover your account with a wallet of your choice.
         </p>
-        <pre>
-          {JSON.stringify(
-            {
-              chainId,
-              connectedAddress: account.address,
-              connectedConnector: account.connector?.id,
-              view,
-            },
-            null,
-            2,
-          )}
-        </pre>
         <section>
           <ol className="flex flex-col gap-3 py-2">
             {connectors.map((connector) => (
               <li key={connector.uid}>
                 <Ariakit.Button
                   className="flex items-center gap-2"
-                  onClick={async (event) => {
-                    event.preventDefault()
-                    setView('loading')
-                    await disconnectAll()
-                    await connect
-                      .connectAsync({ connector })
-                      .then(({ accounts: [address] }) =>
-                        grantAdmin.mutate({
-                          address: portoAccount.address,
-                          chainId,
-                          key: { publicKey: address, type: 'address' },
-                        }),
-                      )
-                      .then(() => setView('success'))
-                      .catch(() => setView('error'))
-                  }}
+                  onClick={async (event) =>
+                    connectThenGrantAdmin(event, connector)
+                  }
                 >
                   <Ariakit.VisuallyHidden>
                     {connector.name}
@@ -145,18 +187,8 @@ function RouteComponent() {
             </Link>
           }
         />
-      </div>
+        <pre>{JSON.stringify(admins, null, 2)}</pre>
+      </section>
     </CatchBoundary>
-  )
-}
-
-function useRouterEvent<T extends keyof RouterEvents>(
-  type: T,
-  handler: (event: RouterEvents[T]) => void,
-) {
-  const router = useRouter()
-  React.useEffect(
-    () => router.subscribe(type, handler),
-    [router, type, handler],
   )
 }
