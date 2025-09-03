95bf2236bdf58fe04b203c936bf107362698eff9
jxom <7336481+jxom@users.noreply.github.com>
2025-09-03 10:58:21 +1000

    refactor: `Tokens` module

--- src/core/internal/schema/capabilities.ts
+++ src/core/internal/schema/capabilities.ts
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 import * as z from 'zod/mini'
+||||||| parent of bc3f8c7c (refactor: `Tokens` module)
+import * as Schema from 'effect/Schema'
+import * as FeeToken from './feeToken.js'
+=======
+import * as Schema from 'effect/Schema'
+>>>>>>> bc3f8c7c (refactor: `Tokens` module)
 import * as Token from '../schema/token.js'
 import * as Permissions from './permissions.js'
 import * as u from './utils.js'
@@ -75,14 +82,41 @@ export namespace signInWithEthereum {
 }
 
 export namespace feeToken {
+<<<<<<< HEAD
   export const GetCapabilitiesResponse = z.object({
     supported: z.boolean(),
     tokens: z.readonly(z.array(Token.Token)),
+||||||| parent of bc3f8c7c (refactor: `Tokens` module)
+  export const GetCapabilitiesResponse = Schema.Struct({
+    supported: Schema.Boolean,
+    tokens: Schema.Array(
+      Schema.Struct({
+        address: Primitive.Address,
+        decimals: Schema.Number,
+        interop: Schema.optional(Schema.Boolean),
+        nativeRate: Schema.optional(Primitive.BigInt),
+        symbol: Schema.String,
+        uid: Schema.String,
+      }),
+    ),
+=======
+  export const GetCapabilitiesResponse = Schema.Struct({
+    supported: Schema.Boolean,
+    tokens: Schema.Array(Token.Token),
+>>>>>>> bc3f8c7c (refactor: `Tokens` module)
   })
   export type GetCapabilitiesResponse = z.infer<typeof GetCapabilitiesResponse>
 
+<<<<<<< HEAD
   export const Request = z.union([Token.Symbol, u.address()])
   export type Request = z.infer<typeof Request>
+||||||| parent of bc3f8c7c (refactor: `Tokens` module)
+  export const Request = Schema.Union(FeeToken.Symbol, Primitive.Address)
+  export type Request = typeof Request.Type
+=======
+  export const Request = Schema.Union(Token.Symbol, Primitive.Address)
+  export type Request = typeof Request.Type
+>>>>>>> bc3f8c7c (refactor: `Tokens` module)
 }
 
 export namespace grantPermissions {
@@ -133,12 +167,32 @@ export namespace merchantUrl {
 }
 
 export namespace requiredFunds {
+<<<<<<< HEAD
   export const GetCapabilitiesResponse = z.object({
     supported: z.boolean(),
     tokens: z.readonly(z.array(Token.Token)),
+||||||| parent of bc3f8c7c (refactor: `Tokens` module)
+  export const GetCapabilitiesResponse = Schema.Struct({
+    supported: Schema.Boolean,
+    tokens: Schema.Array(
+      Schema.Struct({
+        address: Primitive.Address,
+        decimals: Schema.Number,
+        interop: Schema.optional(Schema.Boolean),
+        nativeRate: Schema.optional(Primitive.BigInt),
+        symbol: Schema.String,
+        uid: Schema.String,
+      }),
+    ),
+=======
+  export const GetCapabilitiesResponse = Schema.Struct({
+    supported: Schema.Boolean,
+    tokens: Schema.Array(Token.Token),
+>>>>>>> bc3f8c7c (refactor: `Tokens` module)
   })
   export type GetCapabilitiesResponse = z.infer<typeof GetCapabilitiesResponse>
 
+<<<<<<< HEAD
   export const Request = z.readonly(
     z.array(
       u.oneOf([
@@ -156,6 +210,35 @@ export namespace requiredFunds {
             .check(z.regex(/^\d+(\.\d+)?$/)),
         }),
       ]),
+||||||| parent of bc3f8c7c (refactor: `Tokens` module)
+  export const Request = Schema.Array(
+    OneOf(
+      Schema.Struct({
+        address: Primitive.Address,
+        value: Primitive.BigInt,
+      }),
+      Schema.Struct({
+        symbol: FeeToken.Symbol,
+        value: Schema.Union(
+          Schema.TemplateLiteral(Schema.Number, '.', Schema.Number),
+          Schema.TemplateLiteral(Schema.Number),
+        ).pipe(Schema.pattern(/^\d+(\.\d+)?$/)),
+      }),
+=======
+  export const Request = Schema.Array(
+    OneOf(
+      Schema.Struct({
+        address: Primitive.Address,
+        value: Primitive.BigInt,
+      }),
+      Schema.Struct({
+        symbol: Token.Symbol,
+        value: Schema.Union(
+          Schema.TemplateLiteral(Schema.Number, '.', Schema.Number),
+          Schema.TemplateLiteral(Schema.Number),
+        ).pipe(Schema.pattern(/^\d+(\.\d+)?$/)),
+      }),
+>>>>>>> bc3f8c7c (refactor: `Tokens` module)
     ),
   )
   export type Request = z.infer<typeof Request>
