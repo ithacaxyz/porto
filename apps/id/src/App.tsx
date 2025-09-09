23b226d497d3f4cddcac66f8ce792d5ba8637898
Tom Meagher <tom@meagher.co>
2025-09-09 17:20:25 -0400

    feat: interop send

--- apps/id/src/App.tsx
+++ apps/id/src/App.tsx
@@ -1,7 +1,9 @@
-import { Query } from '@porto/apps'
 import * as UI from '@porto/ui'
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
+import { QueryClient } from '@tanstack/react-query'
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 import { RouterProvider } from '@tanstack/react-router'
+import { Json } from 'ox'
 import { useAccount, WagmiProvider } from 'wagmi'
 
 import * as Router from '~/lib/Router.tsx'
@@ -12,12 +14,26 @@ export function App() {
   return <RouterProvider context={{ account }} router={Router.router} />
 }
 
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 24, // 24 hours
+      queryKeyHashFn: Json.stringify,
+    },
+  },
+})
+
+const persister = createSyncStoragePersister({
+  key: 'porto.id',
+  storage: typeof window !== 'undefined' ? window.localStorage : undefined,
+})
+
 export function Providers(props: React.PropsWithChildren) {
   return (
     <WagmiProvider config={Wagmi.config}>
       <PersistQueryClientProvider
-        client={Query.client}
-        persistOptions={{ persister: Query.persister }}
+        client={client}
+        persistOptions={{ persister }}
       >
         <UI.Ui>{props.children}</UI.Ui>
       </PersistQueryClientProvider>
