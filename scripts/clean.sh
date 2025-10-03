96d873ca2799f2bf93ddc742e381f1f7b2e183fb
Darya <127833442+defiberrys@users.noreply.github.com>
2025-10-03 10:10:54 +0300

    chore: fix folder deletion order with `-depth`

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 
 # remove all node_modules, recursively
-/usr/bin/find . -name "node_modules" -type d -exec rm -rf {} +
+/usr/bin/find . -depth -type d -name "node_modules" -exec rm -rf {} +
 
 # remove all dist, recursively
-/usr/bin/find . -name "dist" -type d -exec rm -rf {} +
+/usr/bin/find . -depth -type d -name "dist" -exec rm -rf {} +
 
 # remove all build, recursively
-/usr/bin/find . -name "build" -type d -exec rm -rf {} +
+/usr/bin/find . -depth -type d -name "build" -exec rm -rf {} +
