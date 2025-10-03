8c311b8980d60feca9c689b53ac5878f906cc3ae
o-az <omaraziz.dev@proton.me>
2025-10-02 17:03:52 -0700

    chore: rm template assets

--- apps/id2/src/routes/__root.tsx
+++ apps/id2/src/routes/__root.tsx
@@ -33,23 +33,6 @@ export const Route = createRootRouteWithContext<{
   head: () => ({
     links: [
       { href: appCss, rel: 'stylesheet' },
-      {
-        href: '/apple-touch-icon.png',
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-      },
-      {
-        href: '/favicon-32x32.png',
-        rel: 'icon',
-        sizes: '32x32',
-        type: 'image/png',
-      },
-      {
-        href: '/favicon-16x16.png',
-        rel: 'icon',
-        sizes: '16x16',
-        type: 'image/png',
-      },
       { color: '#fffff', href: '/site.webmanifest', rel: 'manifest' },
       { href: '/favicon.ico', rel: 'icon' },
     ],
