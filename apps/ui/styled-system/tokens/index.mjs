0a6c3d8364e650dcef74d8fe21d89a5b3f053e04
Pierre Bertet <hi@bpier.re>
2025-10-10 18:34:19 +0100

    include styled-system for apps/ui

---
+++ apps/ui/styled-system/tokens/index.mjs
@@ -0,0 +1,11 @@
+const tokens = {}
+
+export function token(path, fallback) {
+  return tokens[path]?.value || fallback
+}
+
+function tokenVar(path, fallback) {
+  return tokens[path]?.variable || fallback
+}
+
+token.var = tokenVar
---
+++ apps/ui/styled-system/tokens/tokens.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export type Token = string
+
+export type Tokens = {
+[token: string]: string
+} & { [token: string]: never }
+
+export type TokenCategory = "aspectRatios" | "zIndex" | "opacity" | "colors" | "fonts" | "fontSizes" | "fontWeights" | "lineHeights" | "letterSpacings" | "sizes" | "cursor" | "shadows" | "spacing" | "radii" | "borders" | "borderWidths" | "durations" | "easings" | "animations" | "blurs" | "gradients" | "breakpoints" | "assets"
