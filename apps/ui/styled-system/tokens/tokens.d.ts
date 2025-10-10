151ccc7d45952568d51c0228333e51021002ae18
Pierre Bertet <hi@bpier.re>
2025-10-10 18:36:56 +0100

    ignore apps/ui/styled-system again (too many check:types issues)

--- apps/ui/styled-system/tokens/tokens.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-export type Token = string
-
-export type Tokens = {
-[token: string]: string
-} & { [token: string]: never }
-
-export type TokenCategory = "aspectRatios" | "zIndex" | "opacity" | "colors" | "fonts" | "fontSizes" | "fontWeights" | "lineHeights" | "letterSpacings" | "sizes" | "cursor" | "shadows" | "spacing" | "radii" | "borders" | "borderWidths" | "durations" | "easings" | "animations" | "blurs" | "gradients" | "breakpoints" | "assets"
